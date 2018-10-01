38b02ebc7cdb5236fbbdb9acc15f5c1c512bcfb1
Jason Tiao <chtiao@yahoo.com>
2018-09-30 20:47:02 -0500

    Keep testing

--- progfun1-1-recfun/src/main/scala/recfun/Main.scala
+++ progfun1-1-recfun/src/main/scala/recfun/Main.scala
@@ -13,59 +13,58 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = {
-      if (c < 0) {
-        throw new NoSuchElementException("negative column")
-      } else if (c > r) {
-        throw new NoSuchElementException("column larger than row")
-      } else if (c == 0 || c == r) {
-        1
-      } else {
-        pascal(c - 1, r - 1) + pascal(c, r - 1)
-      }
+  def pascal(c: Int, r: Int): Int = {
+    if (c < 0) {
+      throw new NoSuchElementException("negative column")
+    } else if (c > r) {
+      throw new NoSuchElementException("column larger than row")
+    } else if (c == 0 || c == r) {
+      1
+    } else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
     }
+  }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = {
-
-      def balanceCount(chars: List[Char]): Int = {
-        if (chars.isEmpty) {
-          0
-        } else if (chars.head == '(') {
-          balanceCount(chars.tail) + 1
-        } else if (chars.head == ')') {
-          balanceCount(chars.tail) - 1
-        } else {
-          balanceCount(chars.tail)
-        }
+  def balance(chars: List[Char]): Boolean = {
+    def balanceCount(chars: List[Char]): Int = {
+      if (chars.isEmpty) {
+        0
+      } else if (chars.head == '(') {
+        balanceCount(chars.tail) + 1
+      } else if (chars.head == ')') {
+        balanceCount(chars.tail) - 1
+      } else {
+        balanceCount(chars.tail)
       }
-
-      def balanceOrder(chars: List[Char]): Boolean = {
-        if (chars.isEmpty) {
-          true
-        } else {
-          (balanceCount(chars) <= 0) && balanceOrder(chars.tail)
-        }
-      }
-
-      (balanceCount(chars) == 0) && balanceOrder(chars)
     }
+
+    def balanceOrder(chars: List[Char]): Boolean = {
+      if (chars.isEmpty) {
+        true
+      } else {
+        (balanceCount(chars) <= 0) && balanceOrder(chars.tail)
+      }
+    }
+
+    (balanceCount(chars) == 0) && balanceOrder(chars)
+  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = {
+  def countChange(money: Int, coins: List[Int]): Int = {
 
-      if (coins.isEmpty && money == 0) {
-        1
-      } else if (coins.isEmpty) {
-        0
-      } else if (money >= 0) {
-        countChange(money, coins.tail) + countChange(money - coins.head, coins)
-      } else {
-        0
-      }
+    if (coins.isEmpty && money == 0) {
+      1
+    } else if (coins.isEmpty) {
+      0
+    } else if (money >= 0) {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    } else {
+      0
     }
   }
+}
